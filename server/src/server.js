bfded1b9536b8d9cd886981f9d985fd1ee6cb351
Abinesh025 <abi18official@gmail.com>
2026-04-19 16:09:34 +0530

    First commit

--- server/src/server.js
+++ server/src/server.js
@@ -39,6 +39,7 @@ const allowedOrigins = [
   'https://odoo-hackathon-mu.vercel.app',
   'http://localhost:5173',
   'http://localhost:3001',
+  'http://odoo.serveftp.com',
   'https://odoo.serveftp.com'
 ];
 
